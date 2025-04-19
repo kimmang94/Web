eccddea8bc3fe8a312a8f4c76a9454bf36f3b172
KIMHYEOK\borad <kimmang940715@naver.com>
2025-04-20 01:53:05 +0900

    탭 만들기

--- JS/tab.js
+++ JS/tab.js
@@ -1,9 +1,20 @@
-// 1. 버튼 0누르면 클래스 orange 추가 (기존 orange 는 제거) 
+// 1. 버튼 0 누르면
+// - 모든 버튼에 붙은 orange 클래스명 제거
+// - 버튼 0에 orange 클래스명 추가
+// - 모든 div 에 붙은 show 클래스명 제거
+// - div - 에 show 클래스명 추가
 
-$('.tab-button').eq(0).on('click', function(){
-    for (i = 0; i <= $('.tab-button').length; i++)
-    {
-        $('.tab-button').eq(i).removeCalss('orange');
-    }
-    $('.tab-button').eq(0).addClass('orange');
-});
+var tabBtn = $('.tab-button');
+var tabContetn = $('.tab-content');
+var tabCount = tabBtn.length;
+
+for (let i = 0; i <= tabCount; i++){
+    tabBtn.eq(i).on('click', function(){
+    tabBtn.removeClass('orange');
+    tabBtn.eq(i).addClass('orange');
+    tabContetn.removeClass('show');
+    tabContetn.eq(i).addClass('show');
+     });
+}
+
+
