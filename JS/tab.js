97ab6384946981e0ffe515a864694c7b37e06a00
KIMHYEOK\borad <kimmang940715@naver.com>
2025-04-20 02:12:22 +0900

    모달창 검은부분 누르면 닫기

--- JS/tab.js
+++ JS/tab.js
@@ -17,4 +17,32 @@ for (let i = 0; i <= tabCount; i++){
      });
 }
 
+$('#login').on('click',function(){
+    $('.black-bg').addClass('show-modal')
+})
 
+// 모달창 닫기
+$('.black-bg').on('click', function(e){
+    if(e.target == document.querySelector('.black-bg'))
+    {
+        $('.black-bg').removeClass('show-modal');
+    }
+
+})
+
+
+// 로그인 버튼
+$('form').on('submit', function(e){
+    if (document.getElementById('email').value == ''){
+      alert('아이디를 입력해 주세요');  
+      e.preventDefault(); // 폼 전송 막기
+    } else if (document.getElementById('pass').value == '')
+    {
+      alert('비밀번호를 입력해 주세요');
+      e.preventDefault();
+    } else if (document.getElementById('pass').value.length < 6)
+    {
+      alert('7자리 이상으로 해주세요');
+      e.preventDefault();
+    }
+  })
