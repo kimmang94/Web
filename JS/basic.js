986d497c7f46f349e675ad61773d56e298f7d810
kimhyeok_mac <kimmang940715@naver.com>
2025-05-23 00:35:04 +0900

    codeit 자바스크립트 정리

---
+++ JS/basic.js
@@ -0,0 +1,53 @@
+// 덧셈
+console.log(1 + 8);
+
+// 뺄셈
+console.log(6 - 7);
+
+// 곱셈
+console.log(7 * 7);
+
+// 나눗셈
+console.log(5 / 2);
+
+// 나머지
+console.log(7 % 3);
+
+// 거듭제곱
+console.log(2 ** 3);
+
+// 문자열
+console.log("안녕하세요");
+
+console.log('Hello' + 'World');
+
+//예제
+console.log(`한국 영화 역사상 아카데미상을 받은 것은 '기생충'이 처음이다.
+    아리스토텔레스는 "인간은 사회적 동물이다."라고 말했다`);
+
+
+// Boolean
+console.log(2 > 1);
+console.log(2 < 1);
+
+// typeof 연산자
+console.log(typeof ('Hello' + 'World'));
+console.log(typeof (8 - 3));
+
+// not a number
+
+//형 변환
+let x = 123;
+console.log(x);
+console.log(String(x));
+console.log(typeof x);
+console.log(typeof String(x));
+
+console.log('');
+
+// boolean -> 문자
+let y = true;
+console.log(y);
+console.log(String(y));
+console.log(typeof y);
+console.log(typeof String(y));
